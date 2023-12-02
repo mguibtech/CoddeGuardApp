904517f1fb9b1743de8e25e160bef7950b457c76
Marcos Guibson <mguibtech@gmail.com>
2023-12-02 19:21:18 -0400

    feat: Adding custom fonts in android

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import {Text} from './src/components/Text/Text';
+import {View} from 'react-native';
 
 function App(): JSX.Element {
-  return <Text preset="heading">Mamao</Text>;
+  return (
+    <View>
+      <Text style={{fontFamily: 'NovaSquare-Regular', fontSize: 80}}>
+        Teste
+      </Text>
+      <Text preset="heading" style={{fontFamily: 'PlayfairDisplay-Medium'}}>
+        Mamao
+      </Text>
+      <Text preset="heading" style={{fontFamily: 'Outfit-Medium'}}>
+        Mamao
+      </Text>
+    </View>
+  );
 }
 
 export default App;
